0ad636ac8915a29296eca5023cab0d6c8c317c63
Luismi Feijoo <luism.feijoo26@gmail.com>
2020-01-18 19:38:42 +0100

    create public dirs

--- starter-code/app.js
+++ starter-code/app.js
@@ -2,5 +2,17 @@ const express = require("express");
 const hbs = require("hbs");
 const app = express();
 
+app.set("views", __dirname + "/views");
+app.set("view engine", "hbs");
+
+hbs.registerPartials(__dirname + "/views/partials");
+
+app.use("/static", express.static(__dirname + "/public"));
+
+
+app.get('/', (request, response) => {
+  response.render("home");
+});
+
 const port = 3000;
 app.listen(port, () => console.log(`Ready on port ${port}`));
