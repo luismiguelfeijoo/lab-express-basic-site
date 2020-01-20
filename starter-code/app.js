b08b528f13dd4b35b3a5cb41fdc149cdf52ba9de
Luismi Feijoo <luism.feijoo26@gmail.com>
2020-01-20 17:15:02 +0100

    declare data variables in the app.js

--- starter-code/app.js
+++ starter-code/app.js
@@ -8,12 +8,18 @@ app.set("view engine", "hbs");
 //app.use("/scripts", express.static(__dirname + "/scripts"));
 const data = require("./scripts/data.js")
 
-hbs.registerPartials(__dirname + "/views/partials");
+ 
+const hp = data.hp.then( (result) => result)
+const pokemon = data.pokemon.then( (result) => result)
+const sw = data.sw.then( (result) => result)
+
+
+//hbs.registerPartials(__dirname + "/views/partials");
 
 app.use("/static", express.static(__dirname + "/public"));
 
 app.get('/', (request, response) => {
-  response.render("home", {personajes});
+  response.render("home");
 });
 
 const port = 3000;
