34a13f510c77479d2614cf49971c027df21ab9d2
Luismi Feijoo <luism.feijoo26@gmail.com>
2020-01-22 01:54:37 +0100

    Add vara.js and structure to paint harry potter random page

--- starter-code/app.js
+++ starter-code/app.js
@@ -14,23 +14,32 @@ const imageSearch = require("./scripts/image-search")
 hbs.registerPartials(__dirname + "/views/partials");
 
 app.use("/public", express.static(__dirname + "/public"));
+app.use("/scripts", express.static(__dirname + "/scripts"));
+
 
 //created this way to let an image as an active element of the carousel
 const series = {
-  first : "/public/images/hp.jpg",
+  first : {url: "/public/images/hp.jpg", path: "/harrypotter"},
   rest: [
-  {path: "/public/images/sw.png"},
-  {path: "/public/images/pokemon.jpg"}
+  {url: "/public/images/sw.png", path: "/starwars"},
+  {url: "/public/images/pokemon.jpg", path: "/pokemon"}
 ]}
 
 app.get('/', (request, response) => {
-  //response.send(imageSearch.client.search("Harry Potter", imageSearch.options).then( (images) => images).catch(error => console.log(error)) )
-  /*const pokemon = data.pokemon.then( (result) => {
-    console.log(result)
-    response.render("home", {pokemon:result[0]});
-  })
-  */
   response.render("home", series)
+});
+
+
+app.get('/harrypotter', (request, response) => {
+  const hp = data.hp.then( (result) => {
+    console.log(result[0].full)
+    imageSearch.client.search(result[0].full, imageSearch.options).then( (images) => {
+      console.log(images[0])
+      response.render("harrypotter", {hp: result[0], image: images[0].url});
+    }).catch( () => {
+      response.render("harrypotter", {hp: result[0], image: "/public/images/not-found.png"});
+    })
+  })
 });
 
 const port = 3000;
